392466f835836564958a24013742ed96f7c376c2
qsy <2074474233@qq.com>
2025-03-19 09:32:39 +0800

    Update to version 3 of AddTwoNums.py

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,11 +1,19 @@
 # Add Two Numbers in Python
 # Author: Qin Siyuan
-# Using user input
+# Using a function
 
-#taking user input
-a = input("First number:")
-b = input("Second number:")
+# function to add two numbers 
+def add(a,b):
 
-#converting input to floor and adding
-res = float(a) + float(b)
+ #converting input to floor and adding
+ result = float(a) + float(b)
+ return result
+
+# taking user input
+a = input("First Number:")
+b = input("Second Number:")
+
+# calling function
+res = add(a,b)
+print ("The Answer is:")
 print (res)
