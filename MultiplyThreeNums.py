672a41dfa6753920e8de6b757fd24ef09b4986b0
QSY202283890023 <2074474233@qq.com>
2025-03-22 21:15:13 +0800

    Add MultiplyThreeNums.py program

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,11 @@
+//Author: Qin Siyuan
+//Date: 22/3/2025
+def multiply_three_numbers(num1, num2, num3):
+    return num1 * num2 * num3
+
+if __name__ == "__main__":
+   n1 = float(input("Enter first number: "))
+   n2 = float(input("Enter second number: "))
+   n3 = float(input("Enter third number: "))
+   result = multiply_three_numbers(num1, num2, num3)
+   print (f"The product is: {result}")
